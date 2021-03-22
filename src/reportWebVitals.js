39736b66937f92c8cbe798c6ef5d998fa3bcd96e
jd03249 <66363179+jd03249@users.noreply.github.com>
2021-03-22 14:44:36 +0900

    첫 커밋

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,3 +1,5 @@
+//필요없어보이는데 일단 냅둠
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
