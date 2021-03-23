f41bd9a27c583872a3f30ce24e2ca3c2dbd3226f
jd03249 <66363179+jd03249@users.noreply.github.com>
2021-03-23 15:30:55 +0900

    구현 완성

--- src/app.jsx
+++ src/app.jsx
@@ -1,50 +1,71 @@
-import React, { Component } from 'react';
-import './App.css';
-import Habits from './components/habits';
-import Navbar from './components/navbar';
+import React, { Component } from "react";
+import "./App.css";
+import Habits from "./components/habits";
+import Navbar from "./components/navbar";
 
 class App extends Component {
-  state ={
-    habits: [
-        {id: 1, name: 'Reading', count: 0},
-        {id: 2, name: 'Running', count: 0},
-        {id: 3, name: 'Coding', count: 0},
-    ],
-};
-handleIncrement = habit =>{
-  const habits = [...this.state.habits]; //...:아이템들을 새로운 배열에 각각 복사
-  const index = habits.indexOf(habit); //indexOf:배열의 몇번째에 있는지 확인
-  habits[index].count++;
-  this.setState({ habits: habits}); //key: 로컬변수 배열
-};
+    state = {
+        habits: [
+            { id: 1, name: "Reading", count: 0 },
+            { id: 2, name: "Running", count: 0 },
+            { id: 3, name: "Coding", count: 0 },
+        ],
+    };
+    handleIncrement = (habit) => {
+        const habits = [...this.state.habits]; //...:아이템들을 새로운 배열에 각각 복사
+        const index = habits.indexOf(habit); //indexOf:배열의 몇번째에 있는지 확인
+        habits[index].count++;
+        this.setState({ habits: habits }); //key: 로컬변수 배열
+    };
 
-handleDecrement = habit =>{
-  const habits = [...this.state.habits]; //Spread Operator(...)을 이용한 이유->직접 배열의 state를 수정하는것은 좋지 않기 때문
-  const index = habits.indexOf(habit); 
-  const count =  habits[index].count - 1;
-  habits[index].count = count < 0 ? 0 : count;
-  this.setState({ habits }); //key와 로컬변수 배열 이름이 같으면 하나로 생략 가능
-};
+    handleDecrement = (habit) => {
+        const habits = [...this.state.habits]; //Spread Operator(...)을 이용한 이유->직접 배열의 state를 수정하는것은 좋지 않기 때문
+        const index = habits.indexOf(habit);
+        const count = habits[index].count - 1;
+        habits[index].count = count < 0 ? 0 : count;
+        this.setState({ habits }); //key와 로컬변수 배열 이름이 같으면 하나로 생략 가능
+    };
 
-handleDelete =habit =>{
-  const habits = this.state.habits.filter(item => item.id !== habit.id);
-  this.setState({ habits });
-};
+    handleDelete = (habit) => {
+        const habits = this.state.habits.filter((item) => item.id !== habit.id);
+        this.setState({ habits });
+    };
 
-  render() {
-    return (
-        <>
-        <Navbar 
-          totalCount={this.state.habits.filter(item => item.count > 0).length}
-        />
-        <Habits
-          habits={this.state.habits}
-          onIncrement={this.handleIncrement}
-          onDecrement={this.handleDecrement}
-          onDelete={this.handleDelete}
-        />
-        </>
-    )}
+    handleAdd = (name) => {
+        const habits = [
+            ...this.state.habits,
+            { id: Date.now(), name: name, count: 0 },
+        ];
+        this.setState({ habits });
+    };
+
+    handleReset = () => {
+        const habits = this.state.habits.map((habit) => {
+            habit.count = 0;
+            return habit;
+        });
+        this.setState({ habits });
+    };
+    render() {
+        return (
+            <>
+                <Navbar
+                    totalCount={
+                        this.state.habits.filter((item) => item.count > 0)
+                            .length
+                    }
+                />
+                <Habits
+                    habits={this.state.habits}
+                    onIncrement={this.handleIncrement}
+                    onDecrement={this.handleDecrement}
+                    onDelete={this.handleDelete}
+                    onAdd={this.handleAdd}
+                    onReset={this.handleReset}
+                />
+            </>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/habitAddForm.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class HabitAddForm extends Component {
+    formRef = React.createRef();
+    inputRef = React.createRef();
+
+    onSubmit = (event) => {
+        event.preventDefault(); //브라우저의 기본기능(submit시 리프레쉬) 막기
+        const name = this.inputRef.current.value;
+        name && this.props.onAdd(name);
+        //this.inputRef.current.value = "";
+        this.formRef.current.reset();
+    };
+
+    render() {
+        return (
+            <form
+                ref={this.formRef}
+                className="add-form"
+                onSubmit={this.onSubmit}
+            >
+                <input
+                    ref={this.inputRef}
+                    type="text"
+                    className="add-input"
+                    placeholder="Habit"
+                />
+                <button className="add-button">Add</button>
+            </form>
+        );
+    }
+}
+
+export default HabitAddForm;
